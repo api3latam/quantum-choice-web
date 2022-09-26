34684efada3bbc5f843abec36fc809b4155d264a
Dave <david.quinta415@gmail.com>
2022-09-26 15:14:26 -0600

    [ADD] WalletConnect Connector

--- components/wallet/connectors.js
+++ components/wallet/connectors.js
@@ -1,5 +1,12 @@
 import { InjectedConnector } from '@web3-react/injected-connector';
+import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 
 export const injected = new InjectedConnector({
   supportedChainIds: [5, 137]
+});
+
+export const WalletConnect = new WalletConnectConnector({
+ rpcUrl: process.env['NEXT_PUBLIC_ALCHEMY'],
+ bridge: "https://bridge.walletconnect.org",
+ qrcode: true,
 });
