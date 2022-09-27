3b0ddae223c51b52123e8b1da43677a16d0d92e1
Dave <david.quinta415@gmail.com>
2022-09-27 17:47:55 -0600

    [ADD] Play view

---
+++ components/playbanner.js
@@ -0,0 +1,122 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { useEffect } from 'react'
+import { useWeb3React } from '@web3-react/core'
+import { injected } from '../components/wallet/connectors'
+
+const Banner = () => {
+  
+  
+  const { active, account, activate, deactivate } =
+    useWeb3React();
+
+  async function connect() {
+    try {
+      await activate(injected);
+      localStorage.setItem("isWalletConnected", true);
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
+
+  async function disconnect() {
+    try {
+      deactivate();
+      localStorage.setItem("isWalletConnected", false);
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
+
+  useEffect(() => {
+    const connectWalletOnPageLoad = async () => {
+      if (localStorage?.getItem("isWalletConnected") === "true") {
+        try {
+          await activate(injected);
+          localStorage.setItem("isWalletConnected", true);
+        } catch (ex) {
+          console.log(ex);
+        }
+      }
+    };
+    connectWalletOnPageLoad();
+  }, []);
+  
+  return (
+    <div>
+      <div className="banner-area">
+        <section class="breadcrumb-area bc-lottery">
+          {/* <img class="bc-img" src="/images/navbar/096_GoldenAwards1.png" alt=""/>*/}
+
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-12">
+                <h4 class="title">Quantum Choice</h4>
+                <ul class="breadcrumb-list">
+                  {active ? (  <li>
+                    <a  href="#" class="btnsi ml-auto" onClick={disconnect} data-toggle="modal"> Disconnect wallet</a>
+                  </li>):(  <li>
+                    <a  href="#" class="btnsi ml-auto" onClick={connect} data-toggle="modal"> Connect wallet</a>
+                  </li>)}
+                </ul>
+                {active ? (
+                  <p className="h2 text-white">
+                    Connected with <b>{account}</b>
+                  </p>
+                ) : (
+                  <p className="h2 text-white">
+                    Not connected
+                  </p>
+                )}
+              </div>
+            </div>
+            <div className="text-center">
+              <div class="row text-center">
+                <div class="col-lg-12 m-2">
+                  <img src="https://news.bit2me.com/wp-content/uploads/2022/08/nft-marche.jpg" alt="" width={400}/>
+                </div>
+                </div>
+                  <a  href="#" class="btnsi ml-auto"  data-toggle="modal"> MINT NFT</a>
+            </div>
+          </div>
+        </section>
+      </div>
+
+      <section className="lottery-area">
+        
+        <div class="daily-lottery">
+          <div class="container">
+            <div class="row justify-content-center">
+              <div class="col-lg-8 col-md-10">
+                <div class="section-heading">
+					<div><h5 class="subtitle">Try to check out our</h5></div>
+                  
+				  <div><h2 class="title">Daily Choice</h2></div>
+                  <div> <p class="text">
+                    We update our site regularly; more and more winners are
+                    added every day! To locate the most recent winner's
+                    information
+                  </p></div>
+                 
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-lg-12">
+                <div class="draw-time">
+                  <div class="subtitle" id="subtitleid">Lottery Draw Starts In:</div>
+                  <div class="draw-counter">
+                    <div>00D : 00H : 00M : 00S</div>
+                  </div>
+                  <div class="text">To meet Today's challenges</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Banner;
