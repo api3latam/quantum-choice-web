ea179b29f81b22bcee3e13a5abe5641ab4848e91
Dave <david.quinta415@gmail.com>
2022-09-27 23:30:18 -0600

    [UPDATE] Style in /play

--- components/playbanner.js
+++ components/playbanner.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import { useEffect } from 'react'
+import { useEffect, useState} from 'react'
 import { useWeb3React } from '@web3-react/core'
 import { injected } from '../components/wallet/connectors'
 
 const Banner = () => {
-  
+  /**State for image url */
+  /**TODO: Replace with NFT Url */
+    const [imageUrl, setImageUrl] = useState('https://cdn3.f-cdn.com/contestentries/1233731/27978425/5a61f080ea253_thumb900.jpg');
+
   
   const { active, account, activate, deactivate } =
     useWeb3React();
@@ -53,27 +56,27 @@ const Banner = () => {
               <div class="col-lg-12">
                 <h4 class="title">Quantum Choice</h4>
                 <ul class="breadcrumb-list">
+                    
                   {active ? (  <li>
                     <a  href="#" class="btnsi ml-auto" onClick={disconnect} data-toggle="modal"> Disconnect wallet</a>
+                    <p className="h2 text-white">
+                        Connected with <b>{account}</b>
+                      </p>
                   </li>):(  <li>
                     <a  href="#" class="btnsi ml-auto" onClick={connect} data-toggle="modal"> Connect wallet</a>
+                     <p className="h2 text-white">
+                        Not connected
+                      </p>
                   </li>)}
+
                 </ul>
-                {active ? (
-                  <p className="h2 text-white">
-                    Connected with <b>{account}</b>
-                  </p>
-                ) : (
-                  <p className="h2 text-white">
-                    Not connected
-                  </p>
-                )}
+             
               </div>
             </div>
             <div className="text-center">
               <div class="row text-center">
                 <div class="col-lg-12 m-2">
-                  <img src="https://news.bit2me.com/wp-content/uploads/2022/08/nft-marche.jpg" alt="" width={400}/>
+                  <img src={imageUrl} alt="" width={300} height={300}/>
                 </div>
                 </div>
                   <a  href="#" class="btnsi ml-auto"  data-toggle="modal"> MINT NFT</a>
@@ -89,29 +92,19 @@ const Banner = () => {
             <div class="row justify-content-center">
               <div class="col-lg-8 col-md-10">
                 <div class="section-heading">
-					<div><h5 class="subtitle">Try to check out our</h5></div>
+					<div><h5 class="subtitle">QUANTUM CHOICE</h5></div>
                   
 				  <div><h2 class="title">Daily Choice</h2></div>
                   <div> <p class="text">
-                    We update our site regularly; more and more winners are
-                    added every day! To locate the most recent winner's
-                    information
+                    If you get the shinny NFT, contact andres@api.org for a surprise!
                   </p></div>
-                 
+                  <div> <p class="text">
+                    Now you can enter our raffle! 
+                  </p></div>
                 </div>
               </div>
             </div>
-            <div class="row">
-              <div class="col-lg-12">
-                <div class="draw-time">
-                  <div class="subtitle" id="subtitleid">Lottery Draw Starts In:</div>
-                  <div class="draw-counter">
-                    <div>00D : 00H : 00M : 00S</div>
-                  </div>
-                  <div class="text">To meet Today's challenges</div>
-                </div>
-              </div>
-            </div>
+           
           </div>
         </div>
       </section>
