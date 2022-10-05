7f8ad172876f2f8b42b736ca83a6a39c5d0e6e9f
gpablo6 <gpablo6@outlook.com>
2022-10-04 23:08:44 -0600

    Add rpc urls for networks

--- utils/misc/index.js
+++ utils/misc/index.js
@@ -2,10 +2,24 @@ export * from "./ipfs";
 export * from "./signer";
 
 export const networkIds = {
-    137: "polygon",
-    1: "ethereum",
-    42161: "arbitrum",
-    5: "goerli",
-    42161: "arbitrum",
-    30: "rsk"
+    137: {
+        name: "polygon",
+        rpc: "https://polygon-mainnet.g.alchemy.com/v2/"
+    },
+    1: {
+        name: "ethereum",
+        rpc: "https://mainnet.infura.io/v3/"
+    },
+    42161: {
+        name: "arbitrum",
+        rpc: "https://arb-mainnet.g.alchemy.com/v2/"
+    },
+    5: {
+        name: "goerli",
+        rpc: "https://eth-goerli.g.alchemy.com/v2/"
+    },
+    30: {
+        name: "rsk",
+        rpc: "https://rsk.getblock.io/mainnet/?api_key="
+    }
 }
