50f50625fa15f6e69a0f0e4d0e21cf44abbe81d4
Dave <david.quinta415@gmail.com>
2022-10-07 14:04:06 -0600

    Add Documentation button

--- components/csoon.js
+++ components/csoon.js
@@ -17,7 +17,27 @@ const fullbanner = () => {
         height={1000}
         layout="responsive"
         />
+        <div>
+          <a className="btnsi ml-auto" href="https://api3-1.gitbook.io/api-latam-user-documentation/products/randomness-as-a-public-good" target="_blank"> Documentation</a>
+        </div>
+
+       
        </div>
+
+       <div className="cs-logo">
+        <img src="/images/navbar/logo-qrng2.png" alt="Logo1"
+        quality="100"
+        max-width={120}
+        height={100}
+        layout="responsive"/>
+
+        <img src="/images/navbar/banner-qrng1.png" alt="Logo2"
+        quality="100"
+        width={120}
+        height={100}
+        layout="responsive"/>
+
+        </div>
     </div>
   );
 };
