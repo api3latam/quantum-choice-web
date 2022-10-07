5c1fbc3cfa4bac54e6f197ac0e6ac3d3721fa9ac
Dave <david.quinta415@gmail.com>
2022-10-06 20:40:57 -0600

    Hotfix - Env variables Vercel

--- utils/firebase/auth.js
+++ utils/firebase/auth.js
@@ -5,12 +5,12 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 
 const firebaseConfig = {
-  apiKey: process.env['NEXT_PUBLIC_FIREBASE_API'],
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API,
   authDomain: "quantum-choice.firebaseapp.com",
   projectId: "quantum-choice",
   storageBucket: "quantum-choice.appspot.com",
-  messagingSenderId: ["NEXT_PUBLIC_FIREBASE_MESSAGE"],
-  appId: process.env["NEXT_PUBLIC_FIREBASE_ID"],
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_ID,
   measurementId: "G-2HP3ERTHWP"
 };
 
