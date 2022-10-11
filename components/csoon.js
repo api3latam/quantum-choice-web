61d1111e0275578bdce61d58d941f368d15a6956
Dave <david.quinta415@gmail.com>
2022-10-11 12:43:23 -0600

    [ADD] Coming soon page to /play

---
+++ components/csoon.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Image from "next/image";
+
+const fullbanner = () => {
+
+ 
+  
+  return (
+    <div className="bg-image-wrapper">
+
+       <div className="cs-text">
+         <Image
+        src="/images/navbar/cs-text.png"
+        alt="Cartoon graduates jump with happiness"
+        quality="100"
+        width={5000}
+        height={1000}
+        layout="responsive"
+        />
+        <div>
+          <a className="btnsi ml-auto" href="https://api3-1.gitbook.io/api-latam-user-documentation/products/randomness-as-a-public-good" target="_blank"> Documentation</a>
+        </div>
+
+       
+       </div>
+
+      
+    </div>
+  );
+};
+
+export default fullbanner;
