bfc3b94c790a998f5444b70fdb317f55b99cc81b
Dave <david.quinta415@gmail.com>
2022-10-12 16:11:15 -0600

    Functional countdown

--- components/firstbanner.js
+++ components/firstbanner.js
@@ -10,7 +10,34 @@ import { setAddress,
       getSignedHash,
       setSignedHash } from '../utils/queries';
 
+
+
 const Fullbanner = () => {
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  const countDownDate = new Date("Oct 15, 2022 23:59:59").getTime();
+  const countdown = setInterval(() => {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    setDays(days);
+    setHours(hours);
+    setMinutes(minutes);
+    setSeconds(seconds);
+    if (distance < 0) {
+      clearInterval(countdown);
+      setDays(0);
+      setHours(0);
+      setMinutes(0);
+      setSeconds(0);
+    }
+  }, 1000);
 
   return (
     <div>
@@ -32,7 +59,7 @@ const Fullbanner = () => {
                     <h3>Enter to win the first on-chain Quantum Raffle NFT</h3>
                 </div>
                 <div className="banner1-text-support">
-                    <h1 className="count-prin">03D : 08H : 58M : 88S</h1>
+                    <h1 className="count-prin">{days}D : {hours}H : {minutes}M : {seconds}S</h1>
                 </div>
              
               </div>
