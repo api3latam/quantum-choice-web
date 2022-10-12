79a2ad64d1d4851f9ef0cb8725ffda07dadd9e84
gpablo6 <gpablo6@outlook.com>
2022-10-12 14:02:08 -0500

    Add polygon address

--- utils/contracts/index.js
+++ utils/contracts/index.js
@@ -7,15 +7,15 @@ import raffleAbi from "../../public/abis/raffle";
 export function getAddresses(networkName) {
     const addresses = {
         polygon: {
-            nft: "",
+            nft: "0x0904593F8886f20761f9610aBc8789B86ad386f3",
             raffle: ""
         },
         optimism: {
-            nft: "0x0904593F8886f20761f9610aBc8789B86ad386f3",
+            nft: "0xcC0f0d89146ce1B01948E421E90d2aeAa47718f7",
             raffle: ""
         },
         arbitrum: {
-            nft: "0x0904593F8886f20761f9610aBc8789B86ad386f3",
+            nft: "0x6F7951a45B87Df328927e07CEA5771539013B812",
             raffle: ""
         }
     }
