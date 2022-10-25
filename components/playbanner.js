6c4f78b7bae31344022d9b964a07018a2f41708a
Dave <david.quinta415@gmail.com>
2022-10-24 18:00:32 -0600

    [ADD] NFT loader

--- components/playbanner.js
+++ components/playbanner.js
@@ -10,18 +10,20 @@ import { setAddress,
       getTokenStatus,
       getSignedHash,
       setSignedHash } from '../utils/queries';
+import { firestoreClient } from "../utils/queries";
+
       
 const Banner = () => {
   /**State for image url */
   const defaultImageUrl = 'https://firebasestorage.googleapis.com/v0/b/quantum-choice.appspot.com/o/Group%2070.png?alt=media&token=eb21618c-ca07-470f-a37c-88faaf019a79';
+  const NFTLoader ='https://firebasestorage.googleapis.com/v0/b/quantum-choice.appspot.com/o/Comp%201.gif?alt=media&token=be40cf2f-7afc-4323-bd2b-fb4b44e75cbd';
   const [ imageUrl, setImageUrl ] = useState(defaultImageUrl);
   const [ videoUrl, setVideoUrl ] = useState("");
   const [ chainId, setChainId ] = useState(137);
   const [ isInitialized, setInitialization ] = useState(false);
   const [ isVideo, setIsVideo ] = useState(false);
   const [ openseaLink, setOpenseaLink] = useState("");
-  const [ isBeignMinted, setIsBeingMinted ] = useState(false);
-  const [ isMinted, setIsMinted ] = useState(false);
+  const [ isBeignMinted, setIsBeingMinted ] = useState(true);
   const { active, account } = useWeb3React();
 
   let network = "polygon";
@@ -33,6 +35,8 @@ const Banner = () => {
       setChainId(parseInt(chain));
       initialization();
     }
+
+    fetchUserCollection();
   }, []);
 
   const initialization = async () => {
@@ -96,6 +100,40 @@ const Banner = () => {
       console.error(err);
     }
   };
+  //Async function to fetch the user collection
+  const fetchUserCollection = async () => {
+    if(account){
+        try {
+        const netname = getNetworkName(localStorage.getItem('networkId'));
+        console.log(netname);
+        const userCollection = await firestoreClient.collection('users').doc(account).get();
+        const userCollectionData = userCollection.data();
+        
+        if (userCollectionData.signature[netname] && !userCollectionData.minted[netname]) {
+          console.log('Your NFT is being minted');
+          setIsBeingMinted(true);
+        } else if (userCollectionData.minted[netname]) {
+          console.log('You already have an NFT');
+          setIsBeingMinted(false);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    
+  }
+  const getNetworkName = (networkId) => {
+		switch (networkId) {
+			case '137':
+				return 'polygon';
+			case '42161':
+				return 'arbitrum';
+			case '10':
+				return 'optimism';
+			default:
+				return 'polygon';
+		}
+	}
 
     
   // Add the current user's address to the database
@@ -138,21 +176,41 @@ const Banner = () => {
 
           <div class="container">
          
-            <div className="text-center">
+           {isBeignMinted? (
+             <div className="text-center">
               <div class="row text-center">
-                <h1 className="nft-txt">Make your choice</h1>
-                <p className="nft-txt2">Tap to get your NFT</p>
+            
                 <div class="col-lg-12 m-2">
-                  {isVideo ? (<video class="video-fluid z-depth-1" autoplay loop controls muted style={{ width: '250px', height: '400px' }}>
+                  <div>
+                    <h1 className="nft-txt">You made the choice</h1>
+                    <p className="nft-txt2">your NFT will appear here soon</p>
+                      <img src={NFTLoader} alt="" className="img-fluid"/>
+                  </div>
+                  
+                </div>
+                </div>
+                 
+            </div>
+           ):
+            ( <div className="text-center">
+              <div class="row text-center">
+            
+                <div class="col-lg-12 m-2">
+                  <div>
+                    <h1 className="nft-txt">Make your choice</h1>
+                    <p className="nft-txt2">Tap to get your NFT</p>
+                    {isVideo ? (<video class="video-fluid z-depth-1" autoplay loop controls muted style={{ width: '250px', height: '400px' }}>
                     <source src={videoUrl} type="video/mp4" />
-                  </video>):(<img src={imageUrl} alt="" className="img-fluid"/>)}
+                    </video>):(<img src={imageUrl} alt="" className="img-fluid"/>)}
+                  </div>
                   
                 </div>
                 </div>
                   {(imageUrl === defaultImageUrl || (imageUrl === '' && videoUrl === '') ) 
                   ? (<button class="btnsi ml-auto" onClick={addAddress} > MINT NFT</button>) 
                   : (<a class="btn-opensea ml-auto" href={openseaLink} target='_blank'> View on open Sea</a>)} 
-            </div>
+            </div>)
+            }
           </div>
         </section>
       </div>
